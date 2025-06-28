d208ba1a49183f4f032b093a14eb400c53c584de
David martinez <davidmartinezburgos2004@gmail.com>
2025-06-27 21:11:36 -0600

    fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,19 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction(1);
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction(1, 3);
+
+
+        Console.WriteLine($"Fraction 1: {fraction1.GetFraction()}\n Decimal 1:{fraction1.GetDecimal()}");
+        Console.WriteLine($"Fraction 2: {fraction2.GetFraction()}\n Decimal 2:{fraction2.GetDecimal()}");
+        Console.WriteLine($"Fraction 3: {fraction3.GetFraction()}\n Decimal 3:{fraction3.GetDecimal()}");
+        Console.WriteLine($"Fraction 4: {fraction4.GetFraction()}\n Decimal 4:{fraction4.GetDecimal()}");
+
+
+
     }
 }
